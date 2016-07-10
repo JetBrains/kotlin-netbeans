be06cc3b4de690bea38a41bee3ec52a510a44b07
Baratynskiy <baratynskiy@gmail.com>
2016-07-10 13:19:14 +0300

    Removing unused files

--- src/main/java/org/black/kotlin/project/KotlinProjectConstants.java
+++
@@ -1,9 +0,0 @@
-package org.black.kotlin.project;
-
-/**
- * The possible constants of Kotlin project.
- * @author Александр
- */
-public enum KotlinProjectConstants {
-    KOTLIN_SOURCE, JAVA_SOURCE, JAR
-}
