089120ff44c2aab532698df0a62c05fd061c39e3
Baratynskiy <Alexander.Baratynskiy@jetbrains.com>
2016-11-25 11:28:06 +0300

    JavaStubGenerator

---
+++ src/main/java/org/jetbrains/kotlin/filesystem/JavaStubGenerator.kt
@@ -0,0 +1,139 @@
+/*******************************************************************************
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+package org.jetbrains.kotlin.filesystem
+
+import kotlin.Pair
+import org.jetbrains.kotlin.log.KotlinLogger
+import org.jetbrains.org.objectweb.asm.ClassReader
+import org.jetbrains.org.objectweb.asm.Opcodes
+import org.jetbrains.org.objectweb.asm.tree.ClassNode
+import org.jetbrains.org.objectweb.asm.tree.MethodNode
+
+object JavaStubGenerator {
+
+    fun generate(byteCode: ByteArray): Pair<ClassNode, String> {
+        val javaStub = StringBuilder()
+        
+        val classNode = ClassNode()
+        ClassReader(byteCode).accept(classNode, 0)
+        
+        javaStub.append(classNode.packageString)
+        javaStub.append(classNode.classDeclaration())
+        javaStub.append(classNode.methods())
+        
+        javaStub.append("}")
+        return Pair(classNode, javaStub.toString())
+    }
+    
+    val ClassNode.packageString: String
+        get() = "package ${name.substringBeforeLast("/").replace("/", ".")};\n"
+    
+    val ClassNode.className: String
+        get() = name.substringAfterLast("/")
+    
+    private fun ClassNode.classDeclaration(): String {
+        val declaration = StringBuilder()
+        
+        declaration.append(getAccess(access)).append(" ")
+        declaration.append(getFinal(access)).append(" ")
+        declaration.append(getStatic(access)).append(" ")
+        declaration.append(getAbstract(access)).append(" ")
+        declaration.append(getClassType(access)).append(" ")
+        declaration.append(className).append("{\n")
+        
+        return declaration.toString();
+    }
+    
+    private fun ClassNode.methods(): String {
+        val methodsStub = StringBuilder()
+        
+        for (it in methods) {
+            if (it.name == "<init>") continue
+            
+            methodsStub.append(it.getString())
+        }
+        
+        return methodsStub.toString()
+    }
+    
+    private fun MethodNode.getString(): String {
+        val method = StringBuilder()
+        
+        method.append(getAccess(access)).append(" ")
+        method.append(getFinal(access)).append(" ")
+        method.append(getStatic(access)).append(" ")
+        method.append(getAbstract(access)).append(" ")
+        
+        val indexOfRightBracket = desc.indexOf(")")
+        if (indexOfRightBracket == -1) return method.append("void ").append(name).append("(){}").toString()
+        
+        val returnTypeSig = desc.substring(indexOfRightBracket + 1)
+        val returnType = returnTypeSig.replace("/", ".").toType().replace(";", "")
+        
+        method.append(returnType).append(" ").append(name).append("(")
+        
+        val argsSig = desc.substring(1, indexOfRightBracket)
+        if (argsSig.isEmpty()) return method.append("){}").toString()
+        
+        val args = argsSig.split(";")
+        args.forEachIndexed { i, it -> 
+            val argument = "${args[i].replace("/", ".").toType()} a$i"
+            method.append(argument)
+            if (i != args.size - 1) method.append(",")
+        }
+        method.append("){}\n")
+        
+        return method.toString()
+    }
+    
+    private fun getClassType(access: Int) = when {
+        access.contains(Opcodes.ACC_INTERFACE) -> "interface"
+        access.contains(Opcodes.ACC_ENUM) -> "enum"
+        else -> "class"
+    }
+    
+    private fun getAccess(access: Int) = when {
+        access.contains(Opcodes.ACC_PUBLIC) -> "public"
+        access.contains(Opcodes.ACC_PRIVATE) -> "private"
+        access.contains(Opcodes.ACC_PROTECTED) -> "protected"
+        else -> ""
+    }
+   
+    private fun getFinal(access: Int) = if (access.contains(Opcodes.ACC_FINAL)) "final" else ""
+    
+    private fun getStatic(access: Int) = if (access.contains(Opcodes.ACC_STATIC)) "static" else ""
+    
+    private fun getAbstract(access: Int) = if (access.contains(Opcodes.ACC_ABSTRACT)) "abstract" else ""
+    
+    private fun Int.contains(opcode: Int) = (this and opcode) == opcode
+    
+    private fun String.toType() = when {
+        this.startsWith("Z") -> "boolean"
+        this.startsWith("V") -> "void"
+        this.startsWith("B") -> "byte"
+        this.startsWith("C") -> "char"
+        this.startsWith("S") -> "short"
+        this.startsWith("I") -> "int"
+        this.startsWith("J") -> "long"
+        this.startsWith("F") -> "float"
+        this.startsWith("D") -> "double"
+        this.startsWith("L") -> this.substring(1)
+        this.startsWith("[") -> this.substring(1) + "[]"
+        else -> "void"
+    }
+    
+}
--- src/main/java/org/jetbrains/kotlin/filesystem/KotlinVirtualSourceProvider.java
+++ src/main/java/org/jetbrains/kotlin/filesystem/KotlinVirtualSourceProvider.java
@@ -28,6 +28,7 @@ import org.jetbrains.kotlin.resolve.AnalysisResultWithProvider;
 import org.jetbrains.kotlin.resolve.KotlinAnalyzer;
 import org.jetbrains.kotlin.utils.ProjectUtils;
 import org.jetbrains.org.objectweb.asm.ClassReader;
+import org.jetbrains.org.objectweb.asm.Opcodes;
 import org.netbeans.modules.java.preprocessorbridge.spi.VirtualSourceProvider;
 import org.jetbrains.org.objectweb.asm.tree.ClassNode;
 import org.jetbrains.org.objectweb.asm.tree.MethodNode;
@@ -61,12 +62,12 @@ public class KotlinVirtualSourceProvider implements VirtualSourceProvider {
             
             byte[] byteCode = codeList.get(0);
      
-            Pair<String, String> nameAndStub = JavaStubGenerator.generate(byteCode);
-            String classNodeName = nameAndStub.getSecond();
-            String code = nameAndStub.getFirst();
+            Pair<ClassNode, String> nameAndStub = JavaStubGenerator.INSTANCE.generate(byteCode);
+            String code = nameAndStub.getSecond();
             
-            String className = classNodeName.substring(classNodeName.lastIndexOf("/") + 1);
-            String packageName = classNodeName.substring(0, classNodeName.lastIndexOf("/")).replace("/", ".");
+            String className = JavaStubGenerator.INSTANCE.getClassName(nameAndStub.getFirst());
+            String packageName = nameAndStub.getFirst().name
+                    .substring(0, nameAndStub.getFirst().name.lastIndexOf("/")).replace("/", ".");
             
             KotlinLogger.INSTANCE.logInfo(code);
             result.add(normalizedFile, packageName, className, code);
@@ -80,80 +81,6 @@ public class KotlinVirtualSourceProvider implements VirtualSourceProvider {
         AnalysisResultWithProvider result = KotlinAnalyzer.analyzeFile(project, ktFile);
         
         return KotlinLightClassGeneration.INSTANCE.getByteCode(fo, project, result.getAnalysisResult());
-    }
-    
-    private static class JavaStubGenerator {
-        
-        static Pair<String, String> generate(byte[] byteCode) {
-            StringBuilder stubBuilder = new StringBuilder();
-            
-            ClassNode classNode = new ClassNode();
-            new ClassReader(byteCode).accept(classNode, 0);
-            
-            String className = classNode.name.substring(classNode.name.lastIndexOf("/") + 1);
-            String packageName = classNode.name.substring(0, classNode.name.lastIndexOf("/")).replace("/", ".");
-            stubBuilder.append("package ").append(packageName).append("; class ").append(className).append("{");
-            
-            stubBuilder.append(getMethods(classNode));
-            
-            stubBuilder.append("}");
-            return new Pair(stubBuilder.toString(), classNode.name);
-        }
-        
-        private static String getMethods(ClassNode classNode) {
-            StringBuilder methodsStub = new StringBuilder();
-            for (MethodNode method : classNode.methods) {
-                if (method.name.equals("<init>")) continue;
-                String parsedMethodSignature = parseMethodSignature(method.desc, method.name);
-                methodsStub.append(parsedMethodSignature);
-//                KotlinLogger.INSTANCE.logInfo(method.desc);
-                KotlinLogger.INSTANCE.logInfo(parsedMethodSignature);
-            }
-            return methodsStub.toString();
-        }
-        
-        private static String parseMethodSignature(String methodSignature, String methodName) {
-            StringBuilder method = new StringBuilder();
-            method.append("public ");
-            
-            int indexOfRBracket = methodSignature.indexOf(")");
-            if (indexOfRBracket == -1) return "";
-            
-            String returnTypeSig = methodSignature.substring(indexOfRBracket + 1);
-            String returnType = sigTypeToFQNType(returnTypeSig.replace("/", ".")).replace(";", "");
-            
-            method.append(returnType).append(" ").append(methodName).append("(");
-            
-            String argsSig = methodSignature.substring(1, indexOfRBracket);
-            if (argsSig.isEmpty()) {
-                method.append("){};");
-            } else {
-                String[] args = argsSig.split(";");
-                for (int i = 0; i < args.length - 1; i++) {
-                    String argument = sigTypeToFQNType(args[i].replace("/", ".")) + " a" + i;
-                    method.append(argument).append(",");
-                }
-                String argument = sigTypeToFQNType(args[args.length - 1].replace("/", ".")) + " a" + (args.length - 1);
-                method.append(argument).append("){}");
-            }
-            return method.toString();
-        }
-        
-        private static String sigTypeToFQNType(String sigType) {
-            if (sigType.startsWith("Z")) return "boolean";
-            else if (sigType.startsWith("V")) return "void";
-            else if (sigType.startsWith("B")) return "byte";
-            else if (sigType.startsWith("C")) return "char";
-            else if (sigType.startsWith("S")) return "short";
-            else if (sigType.startsWith("I")) return "int";
-            else if (sigType.startsWith("J")) return "long";
-            else if (sigType.startsWith("F")) return "float";
-            else if (sigType.startsWith("D")) return "double";
-            else if (sigType.startsWith("L")) return sigType.substring(1);
-            else if (sigType.startsWith("[")) return sigType.substring(1) + "[]";
-            else return "void";
-        }
-        
     }
     
 }
